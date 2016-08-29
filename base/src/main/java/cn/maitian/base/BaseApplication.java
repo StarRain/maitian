8f8dc5b3925817e25ee60add36a6ffd4f8c2801c
geng <13641194007@139.com>
2016-08-29 18:53:00 +0800

    SocialPlatformInfoActivity

--- base/src/main/java/cn/maitian/base/BaseApplication.java
+++ base/src/main/java/cn/maitian/base/BaseApplication.java
@@ -5,6 +5,7 @@ import cn.maitian.multidex.MultiDexApplication;
 import cn.maitian.retrofit_api.RetrofitApi;
 import cn.maitian.retrofit_api.RetrofitApiUtil;
 import cn.maitian.retrofit_api.RetrofitUtils;
+import cn.maitian.social_sdk.SocialSdk;
 import retrofit2.Retrofit;
 
 /**
@@ -25,6 +26,7 @@ public class BaseApplication extends MultiDexApplication {
         sBaseApplication = this;
         mRetrofit = RetrofitUtils.initClient(this, BuildConfig.BASE_URL, BuildConfig.APPLICATION_ID, BuildConfig.FLAVOR, BuildConfig.VERSION_NAME, String.valueOf(cn.maitian.multidex.BuildConfig.VERSION_CODE));
         mRetrofitApi = RetrofitApiUtil.createRetrofitApi(mRetrofit);
+        initSocial();
     }
 
     public Retrofit getRetrofit() {
@@ -33,5 +35,12 @@ public class BaseApplication extends MultiDexApplication {
 
     public RetrofitApi getRetrofitApi() {
         return mRetrofitApi;
+    }
+
+    private void initSocial() {
+        SocialSdk.init();
+        SocialSdk.setSinaWeibo(BuildConfig.SINA_APPKEY, BuildConfig.SINA_SECRET);
+        SocialSdk.setWeixin(BuildConfig.WEIXIN_APPID, BuildConfig.WEIXIN_SECRET);
+        SocialSdk.setQQZone(BuildConfig.QQ_APPID, BuildConfig.QQ_SECRET);
     }
 }
